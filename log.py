48b3422096def8000f8c09ea1971b10f32f2db95
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-02-03 08:24:20 -0600

    Update log.py

--- log.py
+++ log.py
@@ -1,12 +1,24 @@
 from datetime import datetime
+weblog = False
+try:
+  import extension
+  weblog = True
+except:
+  pass
 file = open("chat_history.log","a")
 def log(message,level="INFO"):
   global file
   now = datetime.now()
   print("[" + now.strftime("%H:%M:%S") + "] [" + level + "] " + message)
+  if weblog:
+    weblogdata(message)
 def logf(message,level="INFO"):
   global file
   now = datetime.now()
   file.write("[" + now.strftime("%H:%M:%S") + "] [" + level + "] " + message + "\n")
   file.close()
   file = open("chat_history.log","a")
+  if weblog:
+    weblogdata(message)
+def weblogdata(datatolog):
+  extension.record(datatolog)
